ae31ed20db659a2e9572ac4100f1e58c8b53f0b9
chunkof <chunkoffset@gmail.com>
2015-08-16 14:26:37 +0900

    クラスの書き方修正

---
+++ src/script/model/store_data.js
@@ -0,0 +1,12 @@
+(function() {
+  "use strict";
+  
+  MyDef.M.StoreData = {};
+  var _data = {};
+  var self = MyDef.M.StoreData;
+  self.SyncToSubjectResult = function(){
+    
+  };
+  self.SyncFromSubjectResult = function(){
+  };
+})();
--- src/script/model/subject_manager.js
+++ src/script/model/subject_manager.js
@@ -2,6 +2,7 @@
   "use strict";
   var entity = undefined;
   //--------------------
+  // MyDef.M.getSubjectManager
   //--------------------
   MyDef.M.getSubjectManager = function(){
     if (entity != undefined){
@@ -11,55 +12,63 @@
     return entity;
   };
   //--------------------
+  // MyDef.M.SubjectManager
   //--------------------
-  MyDef.M.SubjectManager = function(){
-    var self = this;
-    //--------------------
-    self.getBord = function(id){
-      var index = MyDef.Subjects.getIndex(id);
-      var subject = MyDef.Subjects.data[index];
-      if (subject.type == "plane_code"){
-        var id = subject.id;
-        subject = MyDef.BordConverter.planeCodeToBord(subject.code);
-        subject.id = id;
-      }
-      if (subject.stoneColors != undefined){
-        for (var i=0; i<6; ++i){
-          if (subject.stoneColors[i] === undefined){
-            subject.stoneColors.push(MyDef.DefaultStoneColors[i]);
-          }
-        }
-      }else{
-        subject.stoneColors = MyUt.cloneArray(MyDef.DefaultStoneColors);
-      }
-
-      return subject;
-    };
-    //--------------------
-    self.getNum = function(){
-      return MyDef.Subjects.data.length;
-    };
-    //--------------------
-    self.getInfoByIndex = function(index){
-      var subject = MyDef.Subjects.data[index];
-      if (undefined == subject){
-        return undefined;
-      }
-      var cleared = (true == subject.cleared);
-      return {id : subject.id, index: index, cleared: cleared};
-    };
-    //--------------------
-    self.notifyClear = function(id){
-      var index = MyDef.Subjects.getIndex(id);
-      MyDef.Subjects.data[index].cleared = true;
-    };
-    //--------------------
-    self.resetAllResult = function(){
-      for (var i=0; i<MyDef.Subjects.data.length; ++i){
-        MyDef.Subjects.data[i].cleared = undefined;
-      }
-    };
+  var p = function(){ this.initialize();}
+  MyDef.M.SubjectManager = p;
+  //--------------------
+  p.prototype.initialize = function(){
+    this.loadFromStore();
   };
+  //--------------------
+  p.prototype.loadFromStore = function(){
+    //todo;
+  };
+  //--------------------
+  p.prototype.getBord = function(id){
+    var index = MyDef.Subjects.getIndex(id);
+    var subject = MyDef.Subjects.data[index];
+    if (subject.type == "plane_code"){
+      var id = subject.id;
+      subject = MyDef.BordConverter.planeCodeToBord(subject.code);
+      subject.id = id;
+    }
+    if (subject.stoneColors != undefined){
+      for (var i=0; i<6; ++i){
+        if (subject.stoneColors[i] === undefined){
+          subject.stoneColors.push(MyDef.DefaultStoneColors[i]);
+        }
+      }
+    }else{
+      subject.stoneColors = MyUt.cloneArray(MyDef.DefaultStoneColors);
+    }
 
+    return subject;
+  };
+  //--------------------
+  p.prototype.getNum = function(){
+    return MyDef.Subjects.data.length;
+  };
+  //--------------------
+  p.prototype.getInfoByIndex = function(index){
+    var subject = MyDef.Subjects.data[index];
+    if (undefined == subject){
+      return undefined;
+    }
+    var cleared = (true == subject.cleared);
+    return {id : subject.id, index: index, cleared: cleared};
+  };
+  //--------------------
+  p.prototype.notifyClear = function(id){
+    var index = MyDef.Subjects.getIndex(id);
+    MyDef.Subjects.data[index].cleared = true;
+  };
+  //--------------------
+  p.prototype.resetAllResult = function(){
+    for (var i=0; i<MyDef.Subjects.data.length; ++i){
+      MyDef.Subjects.data[i].cleared = undefined;
+    }
+  };
+  
 
 })();
